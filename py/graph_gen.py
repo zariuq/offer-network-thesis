7a5316059cfca57501e63942a8a2bf774b17954e
Zar Goertzel <zariuq@gmail.com>
2017-05-04 20:50:25 +0200

    adde da bunch for shit. Annoying bug. Note, need cython munkres wrapper to actually do matching

---
+++ py/graph_gen.py
@@ -0,0 +1,101 @@
+from neo4j.v1 import GraphDatabase
+import sys
+import numpy as np
+import numpy.random as npr
+import itertools
+import networkx as nx
+from munkres import munkres
+
+def add_task(task_id):
+    return "CREATE (task{0}:Task {{id:\'{0}\'}}) ".format(task_id)
+
+def add_user(user_id):
+    return "CREATE (user{0}:User {{id:\'{0}\'}}) ".format(user_id)
+
+def add_ORnode(node_id, node):
+    offer   = "task" + str(node[0])
+    request = "task" + str(node[1])
+    user    = "user" + str(node[2])
+    node_name = "OR" + "_".join([offer,request,user,str(node_id)])
+    #node_command = "CREATE (ornode:ORnode {{id:\'{1}\', offer:\'{2}\', request:\'{3}\', user:\'{4}\'}}) ".format(node_name, node_id, node[0], node[1], node[2])
+    rel_command = " \n".join(
+        ["MATCH (offer:Task {{id:\'{0}\'}})".format(node[0])
+        ,"MATCH (request:Task {{id:\'{0}\'}})".format(node[1])
+        ,"MATCH (user:Task {{id:\'{0}\'}})".format(node[2])
+        ,"CREATE (ornode:ORnode {{id:\'{1}\', offer:\'{2}\', request:\'{3}\', user:\'{4}\'}})   ".format(node_name, node_id, node[0], node[1], node[2])
+        ,"CREATE (offer)-[:Offer]->(ornode) "
+        ,"CREATE (ornode)-[:Request]->(request) "
+        ,"CREATE (user)-[:Own]->(ornode) "])
+    return rel_command
+
+def generate_er(driver, Ntasks, Nusers, NORnodes):
+    print("Generating Random Erdos Renyi Graph")
+    p = NORnodes / (Ntasks * (Ntasks - 1)) # So the expected # of nodes is correct
+    G = (np.array([*(nx.fast_gnp_random_graph(Ntasks, p, directed=True)).edges()])).transpose()
+    num = G.shape[1]
+    orUsers = npr.randint(Nusers, size=(num))
+    orNodes = list(zip(G[0], G[1], orUsers))
+    generate(driver, Ntasks, Nusers, num, orNodes)
+
+# Generate random Watts Strogatz graph
+# p :- probaility of rewiring edge
+# interesting when Ntasks >> k >> ln(N)
+def generate_wsg(driver, Ntasks, Nusers, k, p):
+    num = int(k * Ntasks) # NORnodes rounded
+    G = (np.array([*(nx.watts_strogatz_graph(Ntasks,k,p)).edges()])).transpose()
+    Gd = (np.array([*(nx.watts_strogatz_graph(Ntasks,k,p)).edges()])).transpose()
+    orUsers = npr.randint(Nusers,size=(num))
+    orNodes = list(zip(G[0], G[1], orUsers[:int(num/2)])) + list(zip(Gd[1], Gd[0], orUsers[int(num/2):]))
+    generate(driver, Ntasks, Nusers, num, orNodes)
+
+def generate_sfg(driver, Ntasks, Nusers):
+    G = (np.array([*nx.scale_free_graph(Ntasks).edges()])).transpose()
+    num = G.shape[1]
+    orUsers = npr.randint(Nusers, size=(num))
+    orNodes = list(zip(G[0], G[1], orUsers))
+    generate(driver, Ntasks, Nusers, num, orNodes)
+
+# #{edges} = 2*(Ntasks - m)*m
+def generate_ba(driver, Ntasks, Nusers, m):
+    G = (np.array([*(nx.barabasi_albert_graph(Ntasks,m)).edges()])).transpose()
+    Gd = (np.array([*(nx.barabasi_albert_graph(Ntasks,m)).edges()])).transpose()
+    num = G.shape[1] + Gd.shape[1]
+    orUsers = npr.randint(Nusers,size=(num))
+    orNodes = list(zip(G[0], G[1], orUsers[:int(num/2)])) + list(zip(Gd[1], Gd[0], orUsers[int(num/2):]))
+    generate(driver, Ntasks, Nusers, num, orNodes)
+
+# #{edges} approx 2*(Ntasks - m)*m
+def generate_plc(driver, Ntasks, Nusers, m, p):
+    G = (np.array([*(nx.powerlaw_cluster_graph(Ntasks,m,p)).edges()])).transpose()
+    Gd = (np.array([*(nx.barabasi_albert_graph(Ntasks,m,p)).edges()])).transpose()
+    num = G.shape[1] + Gd.shape[1]
+    orUsers = npr.randint(Nusers,size=(num))
+    orNodes = list(zip(G[0], G[1], orUsers[:G.shape[1]])) + list(zip(Gd[1], Gd[0], orUsers[G.shape[1]:]))
+    generate(driver, Ntasks, Nusers, num, orNodes)
+
+def generate(driver, Ntasks, Nusers, NORnodes, orNodes):
+    with driver.session().begin_transaction() as tx:
+        print("Creating cypher commands")
+        node_commands = []
+        for i in range(0, Ntasks):
+            node_commands.append(add_task(i+1))
+        for i in range(0, Nusers):
+            node_commands.append(add_user(i+1))
+        node_command = " \n".join(node_commands)
+        print("Running transaction.")
+        tx.run(node_command)
+    print("Creating Task and User indexes.")
+    with driver.session().begin_transaction() as tx:
+        tx.run("CREATE INDEX ON :Task(id) ")
+        tx.run("CREATE INDEX ON :User(id) ")
+    with driver.session().begin_transaction() as tx:
+        print("Creating rel commands")
+        rel_commands = []
+        for i in range(0, NORnodes):
+            rel_commands.append(add_ORnode(i+1, orNodes[i]))
+        print("Running rel transaction")
+        for command in rel_commands:
+            tx.run(command);
+    print("Creating ORnode index")
+    with driver.session().begin_transaction() as tx:
+        tx.run("CREATE INDEX ON :ORnode(id)")
--- py/playpen.py
+++ py/playpen.py
@@ -6,89 +6,12 @@ import numpy as np
 import numpy.random as npr
 import itertools
 import networkx as nx
+from graph_gen import *
 from munkres import munkres
 
 driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "iwin"))
 
 #session = driver.session()
-
-def add_friends(tx, name, friend_name):
-    tx.run("MERGE (a:Person {name: $name}) "
-           "MERGE (a)-[:KNOWS]->(friend:Person {name: $friend_name})",
-           name=name, friend_name=friend_name)
-
-def print_friends(tx, name):
-    for record in tx.run("MATCH (a:Person)-[:KNOWS]->(friend) WHERE a.name = $name "
-                         "RETURN friend.name ORDER BY friend.name ", name=name):
-        print(record["friend.name"])
-
-def add_task(task_id):
-    return "CREATE (task{0}:Task {{id:\'{0}\'}}) ".format(task_id)
-
-def add_user(user_id):
-    return "CREATE (user{0}:User {{id:\'{0}\'}}) ".format(user_id)
-
-def add_ORnode(node_id, node):
-    offer   = "task" + str(node[0])
-    request = "task" + str(node[1])
-    user    = "user" + str(node[2])
-    node_name = "OR" + "_".join([offer,request,user,str(node_id)])
-    node_command = "CREATE ({0}:ORnode {{id:\'{1}\', offer:\'{2}\', request:\'{3}\', user:\'{4}\'}}) ".format(node_name, node_id, node[0], node[1], node[2])
-    rel_command = " \n".join(
-        ["CREATE ({0})-[:Offer]->({1}) ".format(offer, node_name)
-        ,"CREATE ({0})-[:Request]->({1}) ".format(node_name, request)
-        ,"CREATE ({0})-[:Own]->({1}) ".format(user, node_name)])
-    return (node_command, rel_command)
-
-def genRandomNodes(num, num_options, num_users):
-    candidates = npr.randint(num_options,size=(2,num))
-    orUsers = npr.randint(num_users,size=(num))
-    candidates = list(zip(candidates[0], candidates[1], orUsers))
-    #for i in range(0, num):
-    #    while candidates[i][0] == candidates[i][1]: #ensure no same-task offers
-    #        candidates[i] = tuple(np.append(npr.randint(num_options, size=2), candidates[i][2]))
-    diff = 1
-    while diff > 0:
-        candidates = list(set(candidates))
-        diff = num - len(candidates)
-        if diff > 0:
-            candidates + genRandomNodes(diff, num_options, num_users)
-    return candidates
-
-def generate_random(Ntasks, Nusers, NORnodes):
-    print("Generating Random Graph")
-    ornodes = genRandomNodes(NORnodes, Ntasks, Nusers)
-    generate(Ntasks, Nusers, NORnodes, ornodes)
-
-# Generate random Watts Strogatz graph
-# p :- probaility of rewiring edge
-# interesting when Ntasks >> k >> ln(N)
-def generate_wsg(Ntasks, Nusers, NORnodes, p):
-    # n = Ntasks
-    k = 2 * int(NORnodes / Ntasks)
-    num = int((k / 2) * Ntasks) # NORnodes rounded
-    G = (np.array([*(nx.watts_strogatz_graph(Ntasks,k,p)).edges()])).transpose()
-    orUsers = npr.randint(Nusers,size=(num))
-    ornodes = list(zip(G[0], G[1], orUsers))
-    generate(Ntasks, Nusers, num, ornodes)
-
-def generate(Ntasks, Nusers, NORnodes, ornodes):
-    with driver.session().begin_transaction() as tx:
-        print("Creating cypher commands")
-        node_commands = []
-        rel_commands = []
-        for i in range(0, Ntasks):
-            node_commands.append(add_task(i+1))
-        for i in range(0, Nusers):
-            node_commands.append(add_user(i+1))
-        for i in range(0, NORnodes):
-            (nc, rc) = add_ORnode(i+1, ornodes[i])
-            node_commands.append(nc)
-            rel_commands.append(rc)
-        node_command = " \n".join(node_commands)
-        rel_command = " \n".join(rel_commands)
-        print("Running transaction.")
-        tx.run(node_command + rel_command)
 
 # Transform task-centric graph into bipartite graph
 # 1) Generate ORnodes for Offer -> () -> Request, and weight-0 link between them
@@ -102,7 +25,7 @@ def task_to_bipartite():
         #1
         print("Getting offers and requests.")
         result = session.run("match (:Task)-[]-(node:ORnode)-[]->(:Task) "
-                             "return node ")
+                             "return distinct node ")
         for record in result:
             node = record['node']
             offers.append((node['offer'], node['id'], node['user']))
@@ -113,42 +36,56 @@ def task_to_bipartite():
         print("Getting tasks.")
         tasks = dict() # {([offers],[requests])}
         result = session.run("match (task:Task)-[]-(node:ORnode)-[]-(:Task) "
-                             "return task, node ")
+                             "return distinct task, node ")
         for record in result:
             node = record['node']
             task = record['task']['id']
             tasks.setdefault(task,([],[]))
             if node['offer'] == task:
-                tasks[task][0].append(node['offer'] + "_" + node['id'])
+                tasks[task][0].append((node['offer'], node['id']))
             else:
-                tasks[task][1].append(node['request'] + "_" + node['id'])
+                tasks[task][1].append((node['request'], node['id']))
             #print("\n".join("%s: %s" % (key, record[key]) for key in record.keys()))
             #print("\n")
 
-        print("Adding offers, requests, and zero-edges.")
+        print("Adding offers and requests.")
         node_commands = []
-        rel_commands = []
         for offer in offers:
             node_commands.append("CREATE (offer{0}_{1}:Offer {{task:\'{0}\', node_id:\'{1}\', user:\'{2}\'}}) "
                                 .format(offer[0],offer[1],offer[2]))
         for request in requests:
             node_commands.append("CREATE (request{0}_{1}:Request {{task:\'{0}\', node_id:\'{1}\', user:\'{2}\'}}) "
                                 .format(request[0],request[1],request[2]))
-        for edge in zero_weights:
-            rel_commands.append("CREATE ((offer{0}_{2})-[:bNullEdge {{weight:0}}]->(request{1}_{2})) "
-                                .format(edge[0],edge[1],edge[2]))
-                                #,"CREATE ({0})-[:Own]->({1}) ".format(user, node_name)])
-        print("Adding real edges.")
-        for task in tasks.values():
-            for offer, request in itertools.product(task[0], task[1]):
-                rel_commands.append("CREATE ((offer{0})<-[:bEdge {{weight:1}}]-(request{1})) "
-                                    .format(offer, request))
 
         node_command = " \n".join(node_commands)
-        rel_command = " \n".join(rel_commands)
         print("Running transaction.")
         with session.begin_transaction() as tx:
-            tx.run(node_command + rel_command)
+            tx.run(node_command)
+        print("Creating Offer and Request indexes")
+        with session.begin_transaction() as tx:
+            tx.run("CREATE INDEX ON :Offer(node_id) ")
+            tx.run("CREATE INDEX ON :Request(node_id) ")
+
+        rel_commands = []
+        for edge in zero_weights:
+            rel_commands.append("\n".join(
+                ["MATCH (offer:Offer {{task:\'{0}\', node_id:\'{1}\'}}) ".format(edge[0],edge[2])
+                ,"MATCH (request:Request {{task:\'{0}\', node_id:\'{1}\'}}) ".format(edge[1],edge[2])
+                ,"CREATE ((offer)-[:bNullEdge {weight:0}]->(request)) "]))
+        print("Adding zero and real edges.")
+        for task in tasks.values():
+            for offer, request in itertools.product(task[0], task[1]):
+                rel_commands.append("\n".join(
+                    ["MATCH (offer:Offer {{task:\'{0}\', node_id:\'{1}\'}}) "
+                        .format(offer[0],offer[1])
+                    ,"MATCH (request:Request {{task:\'{0}\', node_id:\'{1}\'}}) "
+                        .format(request[0],request[1])
+                    ,"CREATE ((offer)<-[:bEdge {weight:1}]-(request)) "]))
+
+        print("Running relation transaction")
+        with session.begin_transaction() as tx:
+            for command in rel_commands:
+                tx.run(command)
 
 # Use cython munrkes algorithm to find maximum weight matching
 def bmatch():
@@ -165,6 +102,8 @@ def bmatch():
             offers[tuple(record['offer'].values())] = num_offers # store the array #
             iOffers[num_offers] = record['offer']
             num_offers += 1
+            if record['offer']['node_id'] == '53':
+                print(record['offer'])
         result = session.run("match (request:Request) return request ")
         num_requests = 0
         print("Getting requests.")
@@ -172,6 +111,8 @@ def bmatch():
             requests[tuple(record['request'].values())] = num_requests
             iRequests[num_requests] = record['request']
             num_requests += 1
+            if record['request']['node_id'] == '53':
+                print(record['request'])
         print("Getting edges.")
         resultEdge = session.run("match (offer:Offer)-[edge:bEdge]-(request:Request) "
                              "return offer, request, edge ")
@@ -179,6 +120,10 @@ def bmatch():
                              "return offer, request, edge ")
         for result in [resultEdge, resultNullEdge]:
             for record in result:
+                if record['request']['node_id'] == '53':
+                    print(record['request'])
+                if record['offer']['node_id'] == '53':
+                    print(record['offer'])
                 edges.append((tuple(record['offer'].values()), tuple(record['request'].values())
                             ,-record['edge']['weight']))
     size = max(num_offers, num_requests)
@@ -195,7 +140,7 @@ def bmatch():
     #print(edges)
     #for i in range(0,size):
     #    print(B[i])
-    print("Matching.")
+    print("Matching. Size = {0}".format(size))
     matching = munkres(B)
     print("Generating match graph.")
     with driver.session() as session:
@@ -214,6 +159,14 @@ def bmatch():
         with session.begin_transaction() as tx:
             for command in commands:
                 tx.run(command)
+
+def getcycles():
+    with driver.session() as session:
+        print("Getting cycles/matches.")
+        result = session.run("match p=(o:ORnode)-[:Match*1..]->(o) return tail(nodes(p)) ")
+        for record in result:
+            print(record)
+            print("\n")
 
 def delete_all():
     with driver.session() as session:
@@ -244,20 +197,22 @@ def delete_bmatch():
 
 def print_instructions():
     print("Error. Use code.")
-    print("-d to delete the graph. -d a for all, g for graph, b for bipartite, mb for bipartite match")
-    print("-gr [Ntasks Nusers NORnodes] to generate a random graph, [] denotes optionality")
-    print("-gwsg [Ntasks Nusers [NORnodes p]] to generate a random Watts Strogatz graph")
+    print("-d to delete everything. g for graph, b for bipartite, mb for bipartite match")
+    print("-ger [Ntasks Nusers NORnodes] to generate an Erdos-Renyi graph, [] denotes optionality")
+    print("-gwsg [Ntasks Nusers [k p]] to generate a Watts Strogatz graph")
+    print("-gsfg [Ntasks Nusers] to generate a scale free graph")
+    print("-gba [Ntasks Nusers m] to generate a Barabasi Albert graph")
+    print("-gplc [Ntasks Nusers m p] to generate a power law cluster graph")
     print("-tb to transform task-centric graph into a bipartite graph.")
     print("-mb to get maximum weight matching via bipartite graph.")
+    print("-c to get the cycles/matches")
 
 if __name__ == "__main__":
     num_args = len(sys.argv)
     if num_args > 1:
         if sys.argv[1] == "-d":
             if num_args == 3:
-                if sys.argv[2] == "a":
-                    delete_all()
-                elif sys.argv[2] == "g":
+                if sys.argv[2] == "g":
                     delete_graph()
                 elif sys.argv[2] == "b":
                     delete_bipartite()
@@ -265,104 +220,42 @@ if __name__ == "__main__":
                     delete_bmatch()
             else:
                 delete_all()
-        elif sys.argv[1] == "-gr":
+        elif sys.argv[1] == "-ger":
             if num_args == 5:
-                generate_random(int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]))
+                generate_er(driver, int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]))
             else:
-                generate_random(50, 100, 50) #default
+                generate_er(driver, 50, 50, 50) #default
         elif sys.argv[1] == "-gwsg":
             if num_args == 4:
                 Ntasks = int(sys.argv[2])
-                NORnodes = 3*int(Ntasks * np.log(Ntasks)) # so N >> k >> ln(N)
-                generate_wsg(Ntasks, int(sys.argv[3]), NORnodes, 0.5)
+                k = int(4 * np.log(Ntasks)) # so N >> k >> ln(N)
+                generate_wsg(driver, Ntasks, int(sys.argv[3]), k, 0.5)
             elif num_args == 6:
-                generate_wsg(int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]), float(sys.argv[5]))
+                generate_wsg(driver, int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]), float(sys.argv[5]))
             else:
-                generate_wsg(50, 100, 50, 0.5)
+                generate_wsg(driver, 50, 50, 2, 0.5)
+        elif sys.argv[1] == "-gsfg":
+            if num_args == 4:
+                generate_sfg(driver, int(sys.argv[2]), int(sys.argv[3]))
+            else:
+                generate_sfg(driver, 50, 50)
+        elif sys.argv[1] == "-gba":
+            if num_args == 5:
+                generate_ba(driver, int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]))
+            else:
+                generate_ba(driver, 50, 50, 1) #default
+        elif sys.argv[1] == "-gplc":
+            if num_args == 6:
+                generate_plc(driver, int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]), float(sys.argv[5]))
+            else:
+                generate_plc(driver, 50, 50, 1, 0.3) #default
         elif sys.argv[1] == "-tb":
             task_to_bipartite()
         elif sys.argv[1] == "-mb":
             bmatch()
+        elif sys.argv[1] == "-c":
+            getcycles()
         else:
             print_instructions()
     else:
         print_instructions()
-
-
-'''
-
-result = session.run("PROFILE MATCH (p:Person {name: {name}}) "
-                     "RETURN id(p)", {"name": "Arthur"})
-summary = result.consume()
-print(summary.statement_type)
-print(summary.profile)
-
-result = session.run("EXPLAIN MATCH (king), (queen) RETURN king, queen")
-summary = result.consume()
-for notification in summary.notifications:
-    print(notification)
-
-try:
-    session.run("This will cause a syntax error").consume()
-except CypherError:
-    raise RuntimeError("Something really bad has happened!")
-finally:
-    session.close()
-
-result = session.run("MATCH (knight:Person:Knight) WHERE knight.castle = {castle} "
-                     "RETURN id(knight) AS knight_id", {"castle": "Camelot"})
-for record in result:
-    session.run("MATCH (knight) WHERE id(knight) = {id} "
-                "MATCH (king:Person) WHERE king.name = {king} "
-                "CREATE (knight)-[:DEFENDS]->(king)", {"id": record["knight_id"], "king": "Arthur"})
-
-search_term = "Sword"
-result = session.run("MATCH (weapon:Weapon) WHERE weapon.name CONTAINS {term} "
-                     "RETURN weapon.name", {"term": search_term})
-print("List of weapons called %r:" % search_term)
-for record in result:
-    print(record["weapon.name"])
-
-search_term = "Arthur"
-result = session.run("MATCH (weapon:Weapon) WHERE weapon.owner CONTAINS {term} "
-                     "RETURN weapon.name, weapon.material, weapon.size", {"term": search_term})
-print("List of weapons owned by %r:" % search_term)
-for record in result:
-    print(", ".join("%s: %s" % (key, record[key]) for key in record.keys()))
-
-session = driver.session()
-result = session.run("MATCH (knight:Person:Knight) WHERE knight.castle = {castle} "
-                     "RETURN knight.name AS name", {"castle": "Camelot"})
-retained_result = list(result)
-session.close()
-for record in retained_result:
-    print("%s is a knight of Camelot" % record["name"])
-
-
-with session.begin_transaction() as tx:
-    tx.run("MERGE (:Person {name: 'Guinevere'})")
-    tx.success = True
-
-with session.begin_transaction() as tx:
-    tx.run("CREATE (:Person {name: 'Merlin'})")
-    tx.success = False
-
-session.run("MERGE (a:Person {name:'Arthur', title:'King'})")
-
-result = session.run("MATCH (a:Person) WHERE a.name = 'Arthur' RETURN a.name AS name, a.title AS title")
-for record in result:
-    print("%s %s" % (record["title"], record["name"]))
-
-session.close()
-
-with session.begin_transaction() as tx:
-    tx.run("CREATE (:Person {name: 'Merlin'})")
-    tx.success = False
-
-
-with driver.session() as session:
-    session.write_transaction(add_friends, "Arthur", "Guinevere")
-    session.write_transaction(add_friends, "Arthur", "Lancelot")
-    session.write_transaction(add_friends, "Arthur", "Merlin")
-    session.read_transaction(print_friends, "Arthur")
-'''
